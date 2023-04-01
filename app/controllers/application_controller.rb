1cd147212383b4b0c039cf17485d45d3eb543094
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-04-01 19:09:16 +0900

    deviseの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_sign_up_parameters, if: :devise_controller?
+
+  protected
+    def configure_sign_up_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+    end
 end
