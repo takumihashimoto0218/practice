41e893e2c8a5d4560922ff0772aae18e42d3b108
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-04-01 22:19:20 +0900

    ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
+  resources :users
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
