01cb387593db3658c775b6a15043b64b05857052
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-04-03 01:53:26 +0900

    画像を登録できるようにカラム追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
   end
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confrimation)
+      params.require(:user).permit(:name, :email, :password, :password_confrimation, :image)
     end
 end
